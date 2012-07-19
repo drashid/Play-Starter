fd61b6cc2b8bdce340b70e9eb8baf68487dd249f
Daniel Rashid <drashid@squarespace.com>
2012-07-18 21:12:17 -0400

    Underscore js added, simplification of metric flatten using _.flatten, moving metric view/filter options to the top

--- app/assets/js/MetricCtrl.js
+++ app/assets/js/MetricCtrl.js
@@ -10,24 +10,20 @@ function MetricCtrl($scope, $http) {
 	$scope.loadMetrics = function(){
 	  	$http.get('/api/admin/metrics/fetch').success(function(data){
 			//flatten metric array of arrays (one array per web node)
-			var flat = [ ];
-			var k = 0;
-			for(var i = 0; i < data.length; i++){
-	    		for(var j = 0; j < data[i].length; j++){
-	        		flat[k] = data[i][j];
-	        		k++;
-	        	}
-	        }
-			$scope.metrics = flat;
+			$scope.metrics = _.flatten(data);
 	  	});
+	};
+
+	refresh = function() {
+		$scope.loadMetrics();
+		$scope.loadHealth();
 	};
 
 	$scope.clearMachine = function(machineName){
 		$http.post('/api/admin/metrics/clear', {'machine': machineName}).success(function(){
-			$scope.loadMetrics();
-			$scope.loadHealth();
+			refresh();
 		});
-	}
+	};
 
 	sortBy = function(sortFieldName, sortField, orderField) {
 		if($scope[sortField] === sortFieldName){
@@ -36,15 +32,15 @@ function MetricCtrl($scope, $http) {
 			$scope[orderField] = false;
 		}
 		$scope[sortField] = sortFieldName;
-	}
+	};
 
 	$scope.sortTimerBy = function(fieldName){
 		sortBy(fieldName, 'timerSortField', 'timerSortOrder');
-	}
+	};
 
 	$scope.sortMeterBy = function(fieldName){
 		sortBy(fieldName, 'meterSortField', 'meterSortOrder');
-	}
+	};
 
 	//INIT
 	$scope.loadMetrics();
@@ -53,5 +49,5 @@ function MetricCtrl($scope, $http) {
 	$scope.sortMeterBy("name");
 	$scope.timerSortOrder = false;
 	$scope.meterSortOrder = false;
-	
+	$scope.averageNodes = true;
 };
