c84d0e822af51adcb3d9a4f1858b192ae23bae8a
Daniel Rashid <drashid@squarespace.com>
2012-07-21 15:38:13 -0400

    partials html added, routes working sans ng-view embedding of partial html

--- public/application/js/admin.js
+++ public/application/js/admin.js
@@ -1,7 +1,7 @@
 angular.module('admin', []).
   config(function($routeProvider){
     $routeProvider.
-      when('/admin/timers', {
-        template: 'partials/timers.html'
-      })
+      when('/admin/timers', { template: 'partials/timers.html' }).
+      when('/admin/meters', { template: 'partials/meters.html' }).
+      otherwise({ redirectTo:'/admin/health', template: 'partials/health.html'});
   });
