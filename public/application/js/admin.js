8923c62554211b74156bb34470ed0554aea203d7
Daniel Rashid <drashid@squarespace.com>
2012-07-21 19:44:14 -0400

    Making admin page routes not include admin in them as well, e.g. /admin#/admin/health --> /admin#/health

--- public/application/js/admin.js
+++ public/application/js/admin.js
@@ -2,9 +2,9 @@ angular.module('admin', []).
   config(function($routeProvider){
     var assetPath = window.ASSET_PATH;
     $routeProvider.
-      when('/admin/timers', { templateUrl: assetPath + 'application/partials/timers.html', controller: 'MetricCtrl' }).
-      when('/admin/meters', { templateUrl: assetPath + 'application/partials/meters.html', controller: 'MetricCtrl' }).
-      otherwise({ redirectTo:'/admin/health', templateUrl: assetPath + 'application/partials/health.html', controller: 'HealthCtrl'});
+      when('/timers', { templateUrl: assetPath + 'application/partials/timers.html', controller: 'MetricCtrl' }).
+      when('/meters', { templateUrl: assetPath + 'application/partials/meters.html', controller: 'MetricCtrl' }).
+      otherwise({ redirectTo:'/health', templateUrl: assetPath + 'application/partials/health.html', controller: 'HealthCtrl'});
   }).
   filter('round', function(){
     return function(input, digits){
@@ -16,8 +16,8 @@ angular.module('admin', []).
     // set nav button to active on route change
     $scope.$on('$routeChangeSuccess', function() {
       var current = $location.path();
-      $scope.navTimersActive = current == '/admin/timers' ? 'active' : '';
-      $scope.navHealthActive = current == '/admin/health' ? 'active' : '';
-      $scope.navMetersActive = current == '/admin/meters' ? 'active' : '';
+      $scope.navTimersActive = current == '/timers' ? 'active' : '';
+      $scope.navHealthActive = current == '/health' ? 'active' : '';
+      $scope.navMetersActive = current == '/meters' ? 'active' : '';
     });
   };
