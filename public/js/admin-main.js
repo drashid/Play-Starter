9d338bf79cc85835bb2dec03336c4784d9fbfedd
Daniel Rashid <drashid@squarespace.com>
2012-07-22 00:21:34 -0400

    Consolidate admin/admin-main, makes more sense this way.

--- public/js/admin-main.js
+++ public/js/admin-main.js
@@ -8,10 +8,25 @@ require({
     }
   },
   [ 
-    'admin', 
+    'libs/angular', 
     'controller/controllers', 
     'controller/HealthCtrl',
     'controller/MetricCtrl', 
     'controller/AdminCtrl'
-  ]
+  ], 
+  function(angular){
+    angular.module('admin', ['controllers'])
+      .config(function($routeProvider){
+        var assetPath = window.SYSTEM.assetPath;
+        $routeProvider.
+          when('/timers', { templateUrl: assetPath + 'partials/timers.html', controller: 'MetricCtrl' }).
+          when('/meters', { templateUrl: assetPath + 'partials/meters.html', controller: 'MetricCtrl' }).
+          otherwise({ redirectTo:'/health', templateUrl: assetPath + 'partials/health.html', controller: 'HealthCtrl'});
+      })
+      .filter('round', function(){
+        return function(input, digits){
+          return input.toFixed(digits);
+        }
+      });
+  }
 );
