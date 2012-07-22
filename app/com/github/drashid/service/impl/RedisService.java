9ad44127815408b4ba61262d27c1f1c3055f051d
Daniel Rashid <drashid@squarespace.com>
2012-07-22 16:37:32 -0400

    Read redis config from property in production (todo: generalize this)

--- app/com/github/drashid/service/impl/RedisService.java
+++ app/com/github/drashid/service/impl/RedisService.java
@@ -1,11 +1,17 @@
 package com.github.drashid.service.impl;
 
+import java.net.URI;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.Protocol;
 
+import com.github.drashid.config.Environment;
+import com.github.drashid.config.InvalidConfigurationException;
 import com.github.drashid.config.impl.RedisConfig;
 import com.github.drashid.service.AbstractService;
 
@@ -17,9 +23,25 @@ public class RedisService extends AbstractService {
   @Inject
   private RedisConfig config;
 
+  @Inject
+  private Environment env;
+  
   @Override
   protected void startService() {
-    pool = new JedisPool(config.getHost(), config.getPort());
+    if(env == Environment.PRODUCTION){
+      try{ //TODO generalize -- this is heroku specific right now! 
+        URI redisURI = new URI(System.getenv("REDISTOGO_URL"));
+        pool = new JedisPool(new JedisPoolConfig(),
+            redisURI.getHost(),
+            redisURI.getPort(),
+            Protocol.DEFAULT_TIMEOUT,
+            redisURI.getUserInfo().split(":",2)[1]);
+      }catch(Exception e){
+        throw new InvalidConfigurationException("Could not set up Redis from system ENV!"); 
+      }
+    }else{
+      pool = new JedisPool(config.getHost(), config.getPort());
+    }
   }
 
   @Override
