b193c378715b175f12a8baab4b55257c7f44748b
Daniel Rashid <drashid@squarespace.com>
2012-07-22 12:21:23 -0400

    Forget that complicated plugin, just create a task and run it explicitly.  Working with basic params

--- project/Build.scala
+++ project/Build.scala
@@ -4,18 +4,26 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "ProductExplorer"
-    val appVersion      = "1.0-SNAPSHOT"
+  val appName         = "ProductExplorer"
+  val appVersion      = "1.0-SNAPSHOT"
 
-    val appDependencies = Seq(
-      "com.google.inject" % "guice" % "3.0",
-      "com.yammer.metrics" % "metrics-core" % "2.1.2",
-      "com.yammer.metrics" % "metrics-guice" % "2.1.2",
-      "redis.clients" % "jedis" % "2.1.0"
-    )
+  val appDependencies = Seq(
+    "com.google.inject" % "guice" % "3.0",
+    "com.yammer.metrics" % "metrics-core" % "2.1.2",
+    "com.yammer.metrics" % "metrics-guice" % "2.1.2",
+    "redis.clients" % "jedis" % "2.1.0"
+  )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      // Add your own project settings here      
-    )
+  val requireOptimizeTask = TaskKey[Unit]("blahTask")
+
+  val requireJsSettings = requireOptimizeTask := {
+    println("Compiling RequireJS dependencies!")
+    "node project/r.js -o project/app.build.js" !
+  }
+
+  val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
+    requireJsSettings,
+    compile in Compile <<= (compile in Compile).dependsOn(requireOptimizeTask)
+  )
 
 }
---
+++ project/app.build.js
@@ -0,0 +1,11 @@
+//Relative to project directory where r.js lives
+({
+    appDir: "../public",
+    baseUrl: "js",
+    dir: "../public/r-build",
+    modules: [
+        {
+            name: "admin-main"
+        }
+    ]
+})
