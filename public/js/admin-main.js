dfcab9cd3d1a73a1fb47b0129d4a6ba374c9749f
Daniel Rashid <drashid@squarespace.com>
2012-07-22 00:52:52 -0400

    A few comments

--- public/js/admin-main.js
+++ public/js/admin-main.js
@@ -1,4 +1,5 @@
 require({
+    //shims for non-requirejs libraries
     shim: {
       'libs/underscore': { exports: '_' },
       'libs/angular': { exports: 'angular' },
@@ -7,14 +8,17 @@ require({
       'libs/bootstrap-popover': ['libs/bootstrap-tooltip']
     }
   },
-  [ 
+  [ //modules needed for admin panel 
     'libs/angular', 
     'controller/controllers', 
     'controller/HealthCtrl',
     'controller/MetricCtrl', 
     'controller/AdminCtrl'
   ], 
+  //set up front-end routes for single page application (e.g. no page reloading)
   function(angular){
+    //top level module we're going to use in the admin panel,
+    //depends on controllers module our controllers will be added to when they are loaded
     angular.module('admin', ['controllers'])
       .config(function($routeProvider){
         var assetPath = window.SYSTEM.assetPath;
