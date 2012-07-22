7627a8ebc02937c91e5e10c03ee88c2db1b6f262
Daniel Rashid <drashid@squarespace.com>
2012-07-22 18:04:42 -0400

    I keep thinking I've found a good place for the compile optimizer step in the build that won't mess up 'play run' but will run on 'play package', trying another one...

--- project/Build.scala
+++ project/Build.scala
@@ -2,6 +2,9 @@ import sbt._
 import Keys._
 import PlayProject._
 
+import sbt.Defaults._
+import sbt.Package.ManifestAttributes
+
 object ApplicationBuild extends Build {
 
   val appName         = "ProductExplorer"
@@ -12,26 +15,24 @@ object ApplicationBuild extends Build {
     "com.yammer.metrics" % "metrics-core" % "2.1.2",
     "com.yammer.metrics" % "metrics-guice" % "2.1.2",
     "redis.clients" % "jedis" % "2.1.0"
-    // ,
-    // "rhino" % "js" % "1.7R2"
   )
 
-  // val outputPath = SettingKey[File]("assembly-output-path") // + "/public/r-build"
+  val outputPath = artifactPathSetting(artifact);
 
   val requireOptimizeTask = TaskKey[Unit]("optimizejs")
 
-  // val requireJsSettings = requireOptimizeTask <<= outputPath map { (outputPath: File) =>  
-  //   println("Target Path: " + outputPath)
-  //   "java -classpath project/js.jar org.mozilla.javascript.tools.shell.Main project/r.js -o project/app.build.js dir=" + outputPath !
-  // }
-
-  val requireJsSettings = requireOptimizeTask := {  
-    "java -classpath project/js.jar org.mozilla.javascript.tools.shell.Main project/r.js -o project/app.build.js" !
+  val requireJsSettings = requireOptimizeTask <<= outputPath map { (outputPath: File) =>  
+    val path = outputPath.getParent() + "/classes/public/" 
+    "java -classpath project/js.jar org.mozilla.javascript.tools.shell.Main project/r.js -o project/app.build.js dir=" + path !
   }
+
+  // val requireJsSettings = requireOptimizeTask := {  
+  //   "java -classpath project/js.jar org.mozilla.javascript.tools.shell.Main project/r.js -o project/app.build.js" !
+  // }
 
   val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
     requireJsSettings, 
-    (copyResources in Compile) <<= (copyResources in Compile).dependsOn(requireOptimizeTask)
+    (packageBin in Compile) <<= (packageBin in Compile).dependsOn(requireOptimizeTask)
   )
 
 }
