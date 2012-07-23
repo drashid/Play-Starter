22c69d64d0832a3df0d5df0a90b943f3aca53bea
Daniel Rashid <drashid@squarespace.com>
2012-07-22 22:51:25 -0400

    Going to have to figure out a way to pre-minify before sending to Heroku, times out even on the small amount of files currently on build...

--- project/app.build.js
+++ project/app.build.js
@@ -2,25 +2,13 @@
 ({
     appDir: "../public",
     baseUrl: "js",
-    dir: "../public/r-build",
+    dir: "../public/compiled",
     modules: [
         {
             name: "admin-main"
         }
     ],
     optimize: "uglify",
-    // closure: {
-    //   CompilationLevel: "SIMPLE_OPTIMIZATION"
-    // },
-    //WHY DOESN'T THIS WORK!?
-    // uglify: {
-    //   //we require $<named> params in angularjs
-    //   'no_mangle_functions': false,
-    //   no_mangle: false,
-    //   'nm': false,
-    //   nfm: false
-    // },
-
     //external lib definitions, also needed in the modules above
     mainConfigFile: '../public/js/global-config.js' 
 })
