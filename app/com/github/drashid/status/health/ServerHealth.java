acb6b4377618d6368c4f67d5d80699612111d92c
Daniel Rashid <rashid8@gmail.com>
2012-07-24 22:53:55 -0400

    Server Health uses data wrapper class for results, not metrics obj.

--- app/com/github/drashid/status/health/ServerHealth.java
+++ app/com/github/drashid/status/health/ServerHealth.java
@@ -1,12 +1,14 @@
 package com.github.drashid.status.health;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import com.yammer.metrics.core.HealthCheck.Result;
 
 public class ServerHealth {
 
-  private Map<String, Result> checkResults;
+  private Map<String, ServerResult> checkResults;
 
   private long timestamp;
 
@@ -17,16 +19,20 @@ public class ServerHealth {
   }
 
   public ServerHealth(String machine, Map<String, Result> results) {
-    this.checkResults = results;
+    this.checkResults = new HashMap<String, ServerResult>();
+    for (Entry<String, Result> resultEntry : results.entrySet()) {
+      checkResults.put(resultEntry.getKey(), new ServerResult(resultEntry.getValue()));
+    }
+
     this.timestamp = System.currentTimeMillis();
     this.machine = machine;
   }
 
-  public Map<String, Result> getCheckResults() {
+  public Map<String, ServerResult> getCheckResults() {
     return checkResults;
   }
 
-  public void setCheckResults(Map<String, Result> checkResults) {
+  public void setCheckResults(Map<String, ServerResult> checkResults) {
     this.checkResults = checkResults;
   }
 
@@ -44,6 +50,50 @@ public class ServerHealth {
 
   public String getMachine() {
     return machine;
+  }
+
+  public static class ServerResult {
+
+    private boolean healthy;
+
+    private Throwable error;
+
+    private String message;
+
+    public ServerResult(Result value) {
+      this.message = value.getMessage();
+      this.error = value.getError();
+      this.healthy = value.isHealthy();
+    }
+
+    ServerResult() {
+
+    }
+
+    public boolean isHealthy() {
+      return healthy;
+    }
+
+    public void setHealthy(boolean healthy) {
+      this.healthy = healthy;
+    }
+
+    public Throwable getError() {
+      return error;
+    }
+
+    public void setError(Throwable error) {
+      this.error = error;
+    }
+
+    public String getMessage() {
+      return message;
+    }
+
+    public void setMessage(String message) {
+      this.message = message;
+    }
+
   }
 
 }
