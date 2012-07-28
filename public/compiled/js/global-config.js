5d543aed5ff25daed0944c3afedf1c438c620ec6
Daniel Rashid <drashid@squarespace.com>
2012-07-27 22:07:10 -0400

    Deploy generated files.

--- public/compiled/js/global-config.js
+++ public/compiled/js/global-config.js
@@ -1,1 +1,1 @@
-require({shim:{"libs/underscore":{exports:"_"},"libs/angular":{exports:"angular"},"libs/jquery":{exports:"$"},"libs/bootstrap-tooltip":["libs/jquery"],"libs/bootstrap-popover":["libs/bootstrap-tooltip"]}})
+require({shim:{"libs/underscore":{exports:"_"},"libs/angular":{exports:"angular"},"libs/jquery":{exports:"$"},"libs/bootstrap-tooltip":["libs/jquery"],"libs/bootstrap-popover":["libs/bootstrap-tooltip"],"libs/d3":{exports:"d3"},"libs/nv.d3":{exports:"nv",deps:["libs/d3"]}}})
